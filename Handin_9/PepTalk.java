1c4d8c12c1b4100f775f3e8e1ddf3e8e34fa56ea
C-Vilen <chrisvilen@gmail.com>
2022-11-12 14:12:24 +0100

    New PepTalk solution for handin 9

--- Handin_9/PepTalk.java
+++ Handin_9/PepTalk.java
@@ -7,33 +7,75 @@ This was done by C.Vilen
 Due date: 07-11-2022 12:00
 */
 
-// PLEASE BE AWARE THAT IT DOES NOT WORK AS INTENDED
-
 public class PepTalk {
     public static void VowelCheck(String CheckInput) {
-        String CharP = "p";
-        int MatchEnd = 0;
-        Pattern pattern = Pattern.compile("[^aeiou]?[aeiou]((ng|[^aeiou])(?![aeiou]))?", Pattern.CASE_INSENSITIVE);
-        Matcher match = pattern.matcher(CheckInput);
-        while (match.find()) {
-            char FirstChar = match.group().charAt(0);
-            if(FirstChar == 'a' || FirstChar == 'i' || FirstChar == 'o' || FirstChar == 'e' || FirstChar == 'u' ) {
-                System.out.print(CheckInput.substring(MatchEnd, match.end()).trim() + CharP + CheckInput.substring(match.start(), match.end()).trim());
-            } else {
-                System.out.print(CheckInput.substring(MatchEnd, match.end()).trim() + CharP + CheckInput.substring(match.start(), match.end()).substring(1).trim());
+        boolean startWord = true;
+        boolean endWord = false;
+        Pattern charPattern = Pattern.compile("([a-z&&[^aeiou]]*)([aeiou])(?=([a-z&&[^aeiou]]*)([aeiou]*))", Pattern.CASE_INSENSITIVE);
+        Matcher charMatch = charPattern.matcher(CheckInput);
+        Pattern vowelPattern = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE);
+        String outputString = "";
+        String vowelString = "";
+        String charP = "p";
+
+        // Peptalk generator method
+        while (charMatch.find()) {
+            String groupOne = charMatch.group(1);
+            String groupTwo = charMatch.group(2);
+            String groupThree = charMatch.group(3);
+            Boolean groupFourIsEmpty = charMatch.group(4).isEmpty();
+            int groupOneLength = groupOne.length();
+            int groupThreeLength = groupThree.length();
+
+            // Finder for single vowel vords
+            if(startWord && groupFourIsEmpty) {
+                vowelString = groupOne + groupTwo + groupThree;
+                endWord = true;
             }
-            MatchEnd = match.end();
+
+            // Finder for vowel start of the word
+            else if (startWord) {
+                vowelString = groupOne + groupTwo + groupThree.substring(0, groupThreeLength/2);
+                startWord = false;
+                endWord = false;
+            }
+
+            // Finder for vowel end of the word
+            else if(groupFourIsEmpty) {
+                vowelString = groupOne.substring(groupOneLength/2) + groupTwo + groupThree;
+                startWord = true;
+                endWord = true;
+            } 
+
+            // Finder for other types of vowels in words
+            else {
+                vowelString = groupOne.substring(groupOneLength/2) + groupTwo + groupThree.substring(0, groupThreeLength/2);
+                startWord = false;
+                endWord = false;
+            }
+
+            // P repeater method to found word
+            Matcher vowelMatch = vowelPattern.matcher(vowelString.substring(0, 1));
+            if(vowelMatch.matches()) {
+                vowelString += charP + vowelString.toLowerCase();
+                if(endWord) vowelString += " ";
+            } else {
+                vowelString += vowelString.replaceFirst("[a-zA-Z&&[^aeiouAEIOU]]*", charP);
+                if(endWord) vowelString += " ";
+            }
+
+            // Creation of output string based found and repeated words
+            outputString += vowelString;
         }
+        // Printing of output string
+        System.out.println(outputString);
     }
 
+    // Main method for call of Peptalk generator method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);    
         String Input = sc.nextLine();
-        String[] SplitInput = Input.split(" ");
-        for (String Word : SplitInput) {
-            VowelCheck(Word);
-            System.out.print(" ");
-        }
+        VowelCheck(Input);
         sc.close();
     }
 }
---
+++ Handin_9/SimplePepTalk.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// PLEASE BE AWARE THAT IT DOES NOT WORK AS INTENDED
+
+public class SimplePepTalk {
+    public static void VowelCheck(String CheckInput) {
+        String CharP = "p";
+        int MatchEnd = 0;
+        Pattern pattern = Pattern.compile("[^aeiou]?[aeiou]((ng|[^aeiou])(?![aeiou]))?", Pattern.CASE_INSENSITIVE);
+        Matcher match = pattern.matcher(CheckInput);
+        while (match.find()) {
+            char FirstChar = match.group().charAt(0);
+            if(FirstChar == 'a' || FirstChar == 'i' || FirstChar == 'o' || FirstChar == 'e' || FirstChar == 'u' ) {
+                System.out.print(CheckInput.substring(MatchEnd, match.end()).trim() + CharP + CheckInput.substring(match.start(), match.end()).trim());
+            } else {
+                System.out.print(CheckInput.substring(MatchEnd, match.end()).trim() + CharP + CheckInput.substring(match.start(), match.end()).substring(1).trim());
+            }
+            MatchEnd = match.end();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        String Input = sc.nextLine();
+        String[] SplitInput = Input.split(" ");
+        for (String Word : SplitInput) {
+            VowelCheck(Word);
+            System.out.print(" ");
+        }
+        sc.close();
+    }
+}
